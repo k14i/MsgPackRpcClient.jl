73c961af61bf8f5cae5a686c76e137a539a54fca
Keisuke Takahashi <keithseahus@gmail.com>
2015-03-27 08:48:20 +0900

    Add skeleton code for Loop

--- src/loop.jl
+++ src/loop.jl
@@ -10,24 +10,35 @@ type Loop
   attach_periodic_callback::Function
   dettach_periodic_callback::Function
 
-  function Loop()
-    this = new()
+  function Loop(loop=nothing)
+    this                       = new()
+    this.ioloop                = loop # or ioloop.IOLoop()
+    this.ioloop.make_current()
+    this.periodic_callback     = nothing
+
+    this.instance                  = function() instance(this) end
+    this.start                     = function() start(this) end
+    this.stop                      = function() stop(this) end
+    this.attach_periodic_callback  = function() attach_periodic_callback(this) end
+    this.dettach_periodic_callback = function() dettach_periodic_callback(this) end
+
     this
   end
 
-  function instance()
+  function instance(self::Loop)
+    # return Loop(ioloop.IOLoop.current())
   end
 
-  function start()
+  function start(self::Loop)
   end
 
-  function stop()
+  function stop(self::Loop)
   end
 
-  function attach_periodic_callback()
+  function attach_periodic_callback(self::Loop, callback::Callback, callback_time::Int)
   end
 
-  function dettach_periodic_callback()
+  function dettach_periodic_callback(self::Loop)
   end
 
 end
