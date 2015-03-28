728eb81b29c85b9d850a3db9ac571897d1b53ac7
Keisuke TAKAHASHI <keithseahus@gmail.com>
2015-03-28 12:14:13 +0900

    Implement Loop.start, Loop.stop and some features

--- src/loop.jl
+++ src/loop.jl
@@ -11,15 +11,16 @@ type Loop
   dettach_periodic_callback::Function
 
   function Loop(loop=nothing)
-    this                       = new()
-    this.ioloop                = loop # or ioloop.IOLoop()
+    this                        = new()
+    this.ioloop                 = loop # or ioloop.IOLoop()
     this.ioloop.make_current()
-    this.periodic_callback     = nothing
+    this.periodic_callback      = nothing
+    this.periodic_callback_time = 1
 
     this.instance                  = function() instance(this) end
     this.start                     = function() start(this) end
     this.stop                      = function() stop(this) end
-    this.attach_periodic_callback  = function() attach_periodic_callback(this) end
+    this.attach_periodic_callback  = function() attach_periodic_callback(this, this.periodic_callback, this.periodic_callback_time) end
     this.dettach_periodic_callback = function() dettach_periodic_callback(this) end
 
     this
@@ -30,9 +31,11 @@ type Loop
   end
 
   function start(self::Loop)
+    self.ioloop.start()
   end
 
   function stop(self::Loop)
+    self.ioloop.stop()
   end
 
   function attach_periodic_callback(self::Loop, callback::Callback, callback_time::Int)
