d6b28afaba9b57943f7b0805ab76618dc4f866af
Keisuke Takahashi <keithseahus@gmail.com>
2015-04-18 22:54:32 +0900

    Remove unneeded files and codes

--- src/future.jl
+++ src/future.jl
@@ -1,128 +1,128 @@
-module future
-
-export Future
-
-type Loop end
-type Error end
-type Result end
-type Callback end
-type Handler end
-
-type Future
-  loop::Loop
-  error::Error
-  result::Result
-  set_flag::Bool
-  timeout::Int
-  callback::Callback
-  error_handler::Handler
-  result_handler::Handler
-
-  join::Function
-  get::Function
-  set::Function
-  set_result::Function
-  set_error::Function
-  attach_callback::Function
-  attach_error_handler::Function
-  attach_result_handler::Function
-  step_timeout::Function
-
-  function Future(loop, timeout, callback)
-    this                = new()
-    this.loop           = loop
-    this.error          = nothing
-    this.result         = nothing
-    this.set_flag       = false
-    this.timeout        = timeout
-    this.callback       = callback
-    this.error_handler  = nothing
-    this.result_handler = nothing
-
-    this.join                  = function() join(this) end
-    this.get                   = function() get(this) end
-    this.set                   = function() set(this, error, result) end
-    this.set_result            = function() set_result(this, result) end
-    this.set_error             = function() set_error(this, error) end
-    this.attach_callback       = function() attach_callback(this, callback) end
-    this.attach_error_handler  = function() attach_error_handler(this, error_handler) end
-    this.attach_result_handler = function() attach_result_handler(this, result_handler) end
-    this.step_timeout          = function() step_timeout(this) end
-
-    this
-  end
-
-  function join(self::Future)
-    while (!self.set_flag)
-      self.loop.start()
-    end
-  end
-
-  function get(self::Future)
-    self.join(self)
-
-    assert(self.set_flag)
-    if (!self.set_flag)
-      error("RPCError(128)")
-    end
-
-    if (self.result != nothing)
-      if (self.result_handler == nothing)
-        return self.result
-      else
-        self.result_handler(self.result)
-      end
-    else
-      if (self.error != nothing)
-        if (self.error_handler != nothing)
-          self.error_handler(self)
-        end
-      else
-        return self.result
-      end
-    end
-  end
-
-  function set(self::Future, error::Error, result::Result)
-    self.error  = error
-    self.result = result
-
-    if (self.callback)
-      self.callback(self)
-    end
-  end
-
-  function set_result(self::Future, result::Result)
-    self.set(self, nothing, result)
-    self.set_flag = true
-  end
-
-  function set_error(self::Future, error::Error)
-    self.set(self, error, nothing)
-    self.set_flag = true
-  end
-
-  function attach_callback(self::Future, callback::Callback)
-    self.callback = callback
-  end
-
-  function attach_error_handler(self::Future, handler::Hnadler)
-    self.error_handler = handler
-  end
-
-  function attach_result_handler(self::Future, handler::Handler)
-    self.result_handler = handler
-  end
-
-  function step_timeout(self::Future)
-    if self.timeout < 1
-      return true
-    else
-      self.timeout -= 1
-      return false
-    end
-  end
-
-end
-
-end # module future
+# module future
+# 
+# export Future
+# 
+# type Loop end
+# type Error end
+# type Result end
+# type Callback end
+# type Handler end
+# 
+# type Future
+#   loop::Loop
+#   error::Error
+#   result::Result
+#   set_flag::Bool
+#   timeout::Int
+#   callback::Callback
+#   error_handler::Handler
+#   result_handler::Handler
+# 
+#   join::Function
+#   get::Function
+#   set::Function
+#   set_result::Function
+#   set_error::Function
+#   attach_callback::Function
+#   attach_error_handler::Function
+#   attach_result_handler::Function
+#   step_timeout::Function
+# 
+#   function Future(loop, timeout, callback)
+#     this                = new()
+#     this.loop           = loop
+#     this.error          = nothing
+#     this.result         = nothing
+#     this.set_flag       = false
+#     this.timeout        = timeout
+#     this.callback       = callback
+#     this.error_handler  = nothing
+#     this.result_handler = nothing
+# 
+#     this.join                  = function() join(this) end
+#     this.get                   = function() get(this) end
+#     this.set                   = function() set(this, error, result) end
+#     this.set_result            = function() set_result(this, result) end
+#     this.set_error             = function() set_error(this, error) end
+#     this.attach_callback       = function() attach_callback(this, callback) end
+#     this.attach_error_handler  = function() attach_error_handler(this, error_handler) end
+#     this.attach_result_handler = function() attach_result_handler(this, result_handler) end
+#     this.step_timeout          = function() step_timeout(this) end
+# 
+#     this
+#   end
+# 
+#   function join(self::Future)
+#     while (!self.set_flag)
+#       self.loop.start()
+#     end
+#   end
+# 
+#   function get(self::Future)
+#     self.join(self)
+# 
+#     assert(self.set_flag)
+#     if (!self.set_flag)
+#       error("RPCError(128)")
+#     end
+# 
+#     if (self.result != nothing)
+#       if (self.result_handler == nothing)
+#         return self.result
+#       else
+#         self.result_handler(self.result)
+#       end
+#     else
+#       if (self.error != nothing)
+#         if (self.error_handler != nothing)
+#           self.error_handler(self)
+#         end
+#       else
+#         return self.result
+#       end
+#     end
+#   end
+# 
+#   function set(self::Future, error::Error, result::Result)
+#     self.error  = error
+#     self.result = result
+# 
+#     if (self.callback)
+#       self.callback(self)
+#     end
+#   end
+# 
+#   function set_result(self::Future, result::Result)
+#     self.set(self, nothing, result)
+#     self.set_flag = true
+#   end
+# 
+#   function set_error(self::Future, error::Error)
+#     self.set(self, error, nothing)
+#     self.set_flag = true
+#   end
+# 
+#   function attach_callback(self::Future, callback::Callback)
+#     self.callback = callback
+#   end
+# 
+#   function attach_error_handler(self::Future, handler::Hnadler)
+#     self.error_handler = handler
+#   end
+# 
+#   function attach_result_handler(self::Future, handler::Handler)
+#     self.result_handler = handler
+#   end
+# 
+#   function step_timeout(self::Future)
+#     if self.timeout < 1
+#       return true
+#     else
+#       self.timeout -= 1
+#       return false
+#     end
+#   end
+# 
+# end
+# 
+# end # module future
--- src/session.jl
+++ src/session.jl
@@ -1,131 +1,131 @@
-module session
-
-export Session
-
-type Loop end
-type Tcp end
-type Method end
-type Reason end
-type Error end
-type Result end
-
-type Session
-  address::String
-  timeout::Int
-#  loop::Loop
-#  builder::Tcp
-  reconnect_limit::Int
-  pack_encoding::String
-  unpack_encoding::String
-
-  call::Function
-  call_async::Function
-  send_request::Function
-  notify::Function
-  close::Function
-  on_connect_failed::Function
-  on_response::Function
-  on_timeout::Function
-  step_timeout::Function
-  NoSyncIDGenerator::Function
-
-  request_table::Array
-
-  function Session(address, timeout, loop, builder, reconnect_limit, pack_encoding, unpack_encoding)
-    this                   = new()
-    this.address           = address
-    this.timeout           = timeout
-#    this.loop              = loop
-#    this.builder           = builder
-    this.reconnect_limit   = reconnect_limit
-    this.pack_encoding     = pack_encoding
-    this.unpack_encoding   = unpack_encoding
-
-    this.call              = function() call(this, method, args) end
-    this.call_async        = function() call_async(this, method, args) end
-    this.send_request      = function() send_request(this, method, args) end
-    this.notify            = function() notify(this, method, args) end
-    this.close             = function() close(this) end
-    this.on_connect_failed = function() on_connect_failed(self, reason) end
-    this.on_response       = function() on_response(self, msgid, error, result) end
-    this.on_timeout        = function() on_timeout(self, msgid) end
-    this.step_timeout      = function() step_timeout(self) end
-    this.generator         = function() NoSyncIDGenerator() end
-
-    this.request_table     = []
-
-    this
-  end
-
-  function call(self::Session, method::Method, args::Array)
-    # return self.send_request(method, args).get()
-  end
-
-  function call_async(self::Session, method::Method, args::Array)
-    # return self.send_request(method, args)
-  end
-
-  function send_request(self::Session, method::Method, args::Array)
-    # msgid = next(self.generator)
-    # future = Future(self.loop, self.timeout)
-    # self.request_table[msgid] = future
-    # self.transport.send_message([message.REQUEST, msgid, method, args])
-    # return future
-  end
-
-  function notify(self::Session, method::Method, args::Array)
-    # function callback
-    #   self.loop.stop()
-    # end
-    # self.transport.send_message([message.NOTIFY, method, args], callback = callback)
-    # self.loop.start()
-  end
-
-  function close(self::Session)
-    # if self.transport
-    #   self.transport.close()
-    # end
-    # self.transport = nothing
-    # self.request_table = {}
-  end
-
-  function on_connect_failed(self::Session, reason::Reason)
-  end
-
-  function on_response(self::Session, msgid::Int, error::Error, result::Result)
-    # if not msgid in self._request_table
-    #     return
-    # end
-    # future = self.request_table.pop(msgid)
-
-    # if error is not nothing
-    #   future.set_error(error)
-    # else
-    #   future.set_result(result)
-    # end
-    # self.loop.stop()
-  end
-
-  function on_timeout(self::Session, msgid::Int)
-    # future = self.request_table.pop(msgid)
-    # future.set_error("Request timed out")
-  end
-
-  function step_timeout(self::Session)
-  end
-
-  function NoSyncIDGenerator()
-    counter = 0
-    while true
-      counter += 1
-      if counter > (1 << 30)
-        counter = 0
-        break
-      end
-    end
-    return counter
-  end
-
-end
-
-end # module session
+# module session
+# 
+# export Session
+# 
+# type Loop end
+# type Tcp end
+# type Method end
+# type Reason end
+# type Error end
+# type Result end
+# 
+# type Session
+#   address::String
+#   timeout::Int
+# #  loop::Loop
+# #  builder::Tcp
+#   reconnect_limit::Int
+#   pack_encoding::String
+#   unpack_encoding::String
+# 
+#   call::Function
+#   call_async::Function
+#   send_request::Function
+#   notify::Function
+#   close::Function
+#   on_connect_failed::Function
+#   on_response::Function
+#   on_timeout::Function
+#   step_timeout::Function
+#   NoSyncIDGenerator::Function
+# 
+#   request_table::Array
+# 
+#   function Session(address, timeout, loop, builder, reconnect_limit, pack_encoding, unpack_encoding)
+#     this                   = new()
+#     this.address           = address
+#     this.timeout           = timeout
+# #    this.loop              = loop
+# #    this.builder           = builder
+#     this.reconnect_limit   = reconnect_limit
+#     this.pack_encoding     = pack_encoding
+#     this.unpack_encoding   = unpack_encoding
+# 
+#     this.call              = function() call(this, method, args) end
+#     this.call_async        = function() call_async(this, method, args) end
+#     this.send_request      = function() send_request(this, method, args) end
+#     this.notify            = function() notify(this, method, args) end
+#     this.close             = function() close(this) end
+#     this.on_connect_failed = function() on_connect_failed(self, reason) end
+#     this.on_response       = function() on_response(self, msgid, error, result) end
+#     this.on_timeout        = function() on_timeout(self, msgid) end
+#     this.step_timeout      = function() step_timeout(self) end
+#     this.generator         = function() NoSyncIDGenerator() end
+# 
+#     this.request_table     = []
+# 
+#     this
+#   end
+# 
+#   function call(self::Session, method::Method, args::Array)
+#     # return self.send_request(method, args).get()
+#   end
+# 
+#   function call_async(self::Session, method::Method, args::Array)
+#     # return self.send_request(method, args)
+#   end
+# 
+#   function send_request(self::Session, method::Method, args::Array)
+#     # msgid = next(self.generator)
+#     # future = Future(self.loop, self.timeout)
+#     # self.request_table[msgid] = future
+#     # self.transport.send_message([message.REQUEST, msgid, method, args])
+#     # return future
+#   end
+# 
+#   function notify(self::Session, method::Method, args::Array)
+#     # function callback
+#     #   self.loop.stop()
+#     # end
+#     # self.transport.send_message([message.NOTIFY, method, args], callback = callback)
+#     # self.loop.start()
+#   end
+# 
+#   function close(self::Session)
+#     # if self.transport
+#     #   self.transport.close()
+#     # end
+#     # self.transport = nothing
+#     # self.request_table = {}
+#   end
+# 
+#   function on_connect_failed(self::Session, reason::Reason)
+#   end
+# 
+#   function on_response(self::Session, msgid::Int, error::Error, result::Result)
+#     # if not msgid in self._request_table
+#     #     return
+#     # end
+#     # future = self.request_table.pop(msgid)
+# 
+#     # if error is not nothing
+#     #   future.set_error(error)
+#     # else
+#     #   future.set_result(result)
+#     # end
+#     # self.loop.stop()
+#   end
+# 
+#   function on_timeout(self::Session, msgid::Int)
+#     # future = self.request_table.pop(msgid)
+#     # future.set_error("Request timed out")
+#   end
+# 
+#   function step_timeout(self::Session)
+#   end
+# 
+#   function NoSyncIDGenerator()
+#     counter = 0
+#     while true
+#       counter += 1
+#       if counter > (1 << 30)
+#         counter = 0
+#         break
+#       end
+#     end
+#     return counter
+#   end
+# 
+# end
+# 
+# end # module session
